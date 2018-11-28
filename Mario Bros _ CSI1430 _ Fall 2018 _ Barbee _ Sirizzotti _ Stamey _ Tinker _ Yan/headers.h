8d676dc5e5055854374567c53aaebad8941b524e
CarolineTinker <32964598+CarolineTinker@users.noreply.github.com>
2018-11-28 08:23:06 -0600

    new header.h

--- Mario Bros _ CSI1430 _ Fall 2018 _ Barbee _ Sirizzotti _ Stamey _ Tinker _ Yan/headers.h
+++ Mario Bros _ CSI1430 _ Fall 2018 _ Barbee _ Sirizzotti _ Stamey _ Tinker _ Yan/headers.h
@@ -1,14 +1,49 @@
+/*
+ Author:
+ Elisabeth Barbee
+ Ben Sirizzotti
+ Landon Stamey
+ Caroline Tinker
+ Zhenbo Yan
+
+ Assignment Title: Group Project
+ Assignment Description: Mario Bros
+ Due Date: 12/3/18
+ Date Created: 10/?/18
+ Date Last Modified: 11/17/18
+ */
+
 #ifndef HEADERS_H_INCLUDED
 #define HEADERS_H_INCLUDED
 
-#include "constants.h"
-#include "rectangle.h"
-#include "point.h"
-#include "line.h"
+//libraries
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+
+using namespace std;
+
+/*Header Files
+#include "background.h"
+#include "blocks.h"
+#include "chimney.h"
 #include "circle.h"
-#include "SDL_Plotter.h"
 #include "color.h"
+#include "constants.h"
 #include "enemies.h"
+#include "fireplace.h"
 #include "font.h"
+#include "LettersDisplay.h"
+#include "line.h"
+#include "point.h"
+#include "rectangle.h"
+#include "SDL_Plotter.h"
+#include "snowman.h"
+*/
+
 
 #endif // HEADERS_H_INCLUDED
